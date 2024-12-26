5c4ac40d92ddb244f8c7ca439af53b83baf92ab9
Minimalist30 <kouki4122@icloud.com>
2024-12-27 01:22:22 +0900

    ColorButton コンポーネントのボタンサイズを変更し、間隔を調整

--- src/components/ColorButton.tsx
+++ src/components/ColorButton.tsx
@@ -11,14 +11,14 @@ type ColorButtonProps = {
 
 const ColorButton = ({ setTextColor }: ColorButtonProps) => {
   return (
-    <div className="mt-8 flex gap-2">
+    <div className="mt-8 flex gap-3">
       <button
-        className="w-8 h-8 rounded-full border-2 border-white"
+        className="w-7 h-7 rounded-full border-2 border-white"
         style={{ backgroundColor: TEXT_COLORS.WHITE }}
         onClick={() => setTextColor(TEXT_COLORS.WHITE)}
       />
       <button
-        className="w-8 h-8 rounded-full border-2 border-white"
+        className="w-7 h-7 rounded-full border-2 border-white"
         style={{ backgroundColor: TEXT_COLORS.BLUE }}
         onClick={() => setTextColor(TEXT_COLORS.BLUE)}
       />
